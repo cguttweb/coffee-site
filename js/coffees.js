5093eb4390348fc244bbc7260713df0d8375c975
Chloe <cguttweb@pm.me>
2021-09-09 21:25:07 +0100

    adding content

---
+++ js/coffees.js
@@ -0,0 +1,121 @@
+/* eslint-disable prettier/prettier */
+export default [
+  {
+    name: "espresso",
+    description:
+      "strong coffee with a good crema forms the base of many other drinks",
+    photo: "/images/espresso.png",
+    type: 'hot'
+  },
+  {
+    name: "ristretto",
+    description:
+      "Ristretto means restricted or concentrated in Italian so this drink is a short shot of espresso made with half the amount of water compared to an espresso. It is seen as the perfect espresso by some.",
+    photo: "/images/ristretto.png",
+    type: 'hot'
+  },
+  {
+    name: "latte",
+    description:
+      "A drink made with espresso and steamed milk. Can be made with different types of milk e.g. soya, almond or coconut.",
+    photo: "/images/latte.png",
+    type: 'hot'
+  },
+  {
+    name: "cappuccino",
+    description:
+      "An espresso based drink with steamed milk foam it originates from Italy. The name is thought to come from the Capuchin friars referring to the colour of their habitats. Also available as an iced option.",
+    photo: "/images/cappuccino.png",
+    type: 'hot'
+  },
+  {
+    name: "mocha",
+    description:
+      "made with espresso and milk like a latte but with added chocolate flavouring usually cocoa powder. Often available as an iced option and in some cases there is a sweeter white chocolate mocha available.",
+    photo: "/images/mocha.png",
+    type: 'hot'
+  },
+  {
+    name: "cortado",
+    description:
+      "espresso with equal amount of steamed warm milk reducing the acidity. It originates from Spain.",
+    photo: "/images/cortado.png",
+    type: 'hot'
+  },
+  {
+    name: "americano",
+    description:
+      "Drink prepared by diluting an espresso with hot water resulting in a different flavour to traditionally brewed coffee.",
+    photo: "/images/americano.png",
+    type: 'hot'
+  },
+  {
+    name: "macchiato",
+    description:
+      "In Italian macchiato means stained or spotted referring to the small amount of frothed milk used. Espresso coffee topped with a small amount of frothed milk.",
+    photo: "/images/espresso-macchiato.png",
+    type: 'hot'
+  },
+  {
+    name: "espresso con panna",
+    description:
+      "means espresso with cream in Italian a single or double espresso topped with whipped cream",
+    photo: "/images/con-panna.png",
+    type: 'hot'
+  },
+  {
+    name: "piccolo",
+    description:
+      "Australian in origin it is a ristretto shot (20ml) topped with warm milk in a small latte glass (demitasse)",
+    photo: "/images/piccolo.png",
+    type: 'hot'
+  },
+  {
+    name: "flat white",
+    description: "Originally from New Zealand",
+    photo: "/images/flat-white.png",
+    type: 'hot'
+  },
+  {
+    name: "lungo",
+    description:
+      "Lungo means long in Italian made with an espresso machine but with twice as much water. Less strong but more bitter than an espresso",
+    photo: "lungo.png",
+    type: 'hot'
+  },
+  {
+    name: "affogato",
+    description:
+      "Italian for drowned this is a coffee-based dessert with vanilla ice-cream topped or drowned with a shot of hot espresso",
+    photo: "/images/affogato.png",
+    type: 'cold'
+  },
+  {
+    name: "Moretta fanese",
+    description:
+      "It is strong and sweet, and usually drunk after meals as a digestive, or used as a hot drink on cold afternoons. The hot cocktail is a blend of anise, rum and brandy in roughly equal parts.",
+    photo: "/images/moretta.png",
+    type: 'alcoholic'
+  },
+  {
+    name: "espresso martini",
+    description:
+      "a cold coffee-flavoured cocktail made with vodka, espresso, coffee liquer and sugar syrup garnished with coffee beans. It is not a true martini which traditionally includes Tia Maria or Kahlua.",
+    photo: "/images/espresso-martini.png",
+    type: 'alcoholic'
+  },
+  {
+    name: "cold brew",
+    description:
+      "coarse grind coffee is soaked overnight (sometimes longer) in cold water. It produces a drink with less acidity and therefore a sweeter taste.",
+    photo: "/images/cold-brew.png",
+    type: 'cold'
+  },
+  {
+    name: "iced coffee",
+    description:
+      "this differs from cold brew as the coffee is brewed hot and poured over ice. It is brewed stronger to make up for dilution caused by the ice.",
+    photo: "/images/iced-coffee.png",
+    type: 'cold'
+  },
+];
